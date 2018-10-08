7af6bb4c11cd14e79a8e5e5dead3d12d3f53dda3
Hossein <h.beig@yahoo.com>
2018-10-08 20:11:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-document.querySelector('#get-jokes').addEventListener('click', getJokes);
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
-    const number = document.querySelector('input[type="number"]').value;
+  const number = document.querySelector('input[type="number"]').value;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+  const xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-        if (this.status === 200) {
-            const response = JSON.parse(this.responseText);   
-            
-            let output = '';
-            if (response.type === 'success') {
-                response.value.forEach(function(joke){
-                    output += `<br><li>${joke.joke}</li><hr>`;
-                });
-            } else {
-                output += '<li>Somting Went Wrong</li>';
-            }
-            document.querySelector('.jokes').innerHTML = output;
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
-        }
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
     }
-    xhr.send();
-    
-e.preventDefault();    
+  }
+
+  xhr.send();
+
+  e.preventDefault();
 }
