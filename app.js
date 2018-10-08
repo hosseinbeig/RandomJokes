bfffad8ab533d225a63187c438441aaeac96f1f5
Hossein <h.beig@yahoo.com>
2018-10-08 20:14:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function getJokes(e) {
                     output += `<br><li>${joke.joke}</li><hr>`;
                 });
             } else {
-                output += '<li>Somting Went Wrong<li/>'
+                output += '<li>Somting Went Wrong</li>';
             }
             document.querySelector('.jokes').innerHTML = output;
 
