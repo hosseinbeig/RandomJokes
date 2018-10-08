b489d739bea601f07ccf103aaa65e65034531357
Hossein <h.beig@yahoo.com>
2018-10-08 19:43:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.querySelector('#get-jokes').addEventListener('click', getJokes);
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
     const number = document.querySelector('input[type="number"]').value;
